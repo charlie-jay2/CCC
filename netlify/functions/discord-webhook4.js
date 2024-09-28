e23bb7dffbaa0431048e224467826b71694a4abc
FlexaGroup <162819959+FlexaGroup@users.noreply.github.com>
2024-09-28 13:05:46 +0100

    dwh4

--- netlify/functions/discord-webhook4.js
+++ netlify/functions/discord-webhook4.js
@@ -2,15 +2,14 @@ const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
     try {
-        console.log('Received event:', event); // Log the entire event for debugging
         const data = JSON.parse(event.body);
         const applicationCode = Math.floor(10000000 + Math.random() * 90000000).toString();
 
         const embed = {
             embeds: [
                 {
-                    title: `New Application: ${data.name}`,
-                    description: `**Teaching Name:** ${data.name}\n**Date Start:** ${data.start}\n**Date End:** ${data.end}\n**Reason:** ${data.reason}`,
+                    title: `Teaching Name${data.name}`,
+                    description: `**Start Date:** ${data.start}\n**End Date:** ${data.end}\n**Reason for LOA:** ${data.reason}`,
                     color: 3066993,
                     footer: {
                         text: `Application Code: ${applicationCode}`
@@ -36,7 +35,6 @@ exports.handler = async function (event, context) {
             body: JSON.stringify({ message: 'Contact submitted successfully!' })
         };
     } catch (error) {
-        console.error('Error:', error); // Log the error for debugging
         return {
             statusCode: 500,
             body: JSON.stringify({ error: error.message })
