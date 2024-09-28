f8e336a63665d455f29fc9e8256ddfac37434032
FlexaGroup <162819959+FlexaGroup@users.noreply.github.com>
2024-09-28 12:07:48 +0100

    login update?

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,7 +1,25 @@
 // netlify/functions/login.js
 exports.handler = async function (event, context) {
-    // Parse the request body
-    const { username, password } = JSON.parse(event.body);
+    // Check if the method is POST
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ success: false, message: 'Method not allowed' }),
+        };
+    }
+
+    let body;
+    try {
+        // Attempt to parse the JSON body
+        body = JSON.parse(event.body);
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ success: false, message: 'Invalid JSON' }),
+        };
+    }
+
+    const { username, password } = body;
 
     // Fetch environment variables
     const storedUsername = process.env.USER1N;
